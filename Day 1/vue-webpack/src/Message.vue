8304affc9272dca7bf731ec9936655b17c92692d
ihaseeb <ihaseeb98@gmail.com>
2017-09-06 14:44:21 +0500

    End lecture 8-9

---
+++ Day 1/vue-webpack/src/Message.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+      <h1>This is a message!</h1>
+      <app-input></app-input>
+  </div>
+</template>
+<script>
+    import Input from './input.vue';
+    export default {
+        components: {
+            'app-input': Input
+        }
+    }
+</script>
