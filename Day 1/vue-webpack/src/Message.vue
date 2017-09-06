82e5371605c92a57949ca56db5746cc8dd3dd867
ihaseeb <ihaseeb98@gmail.com>
2017-09-06 15:05:16 +0500

    End lecture 10

--- Day 1/vue-webpack/src/Message.vue
+++ Day 1/vue-webpack/src/Message.vue
@@ -1,12 +1,17 @@
 <template>
   <div>
-      <h1>This is a message!</h1>
-      <app-input></app-input>
+      <h1>{{message}}</h1>
+      <app-input :msg="message" @messageChanged="message = $event"></app-input></app-input>
   </div>
 </template>
 <script>
     import Input from './input.vue';
     export default {
+        data() {
+            return {
+                message: 'This is a message!'
+            }
+        },
         components: {
             'app-input': Input
         }
