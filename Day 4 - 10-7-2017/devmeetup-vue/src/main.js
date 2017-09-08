a212f8b3b1ed9b173a8645a596064c756f41d8e3
ihaseeb <ihaseeb98@gmail.com>
2017-09-08 12:32:07 +0500

    End Lecture 13-14 of Vue, Vuetify, Firebase project

--- Day 4 - 10-7-2017/devmeetup-vue/src/main.js
+++ Day 4 - 10-7-2017/devmeetup-vue/src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify'
 import App from './App'
+import * as firebase from 'firebase'
 import router from './router'
 import { store } from './store'
 import DateFilter from './filters/date'
@@ -15,5 +16,14 @@ new Vue({
     el: '#app',
     router,
     store,
-    render: h => h(App)
+    render: h => h(App),
+    created() {
+        firebase.initializeApp({
+            apiKey: 'AIzaSyCTf3BvGyzXSJYiPVtc3IF2wRxHj0yd17M',
+            authDomain: 'devmeetup-ef986.firebaseapp.com',
+            databaseURL: 'https://devmeetup-ef986.firebaseio.com',
+            projectId: 'devmeetup-ef986',
+            storageBucket: 'devmeetup-ef986.appspot.com',
+        })
+    }
 })
