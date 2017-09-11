810e152af8c7ab546b223afea810d3cb044bc5fe
ihaseeb <ihaseeb98@gmail.com>
2017-09-11 13:12:04 +0500

    End Lecture 21 of Vue, Vuetify, Firebase project

--- Day 4 - 10-7-2017/devmeetup-vue/src/main.js
+++ Day 4 - 10-7-2017/devmeetup-vue/src/main.js
@@ -7,6 +7,8 @@ import { store } from './store'
 import DateFilter from './filters/date'
 import AlertCmp from './components/Shared/ALert.vue'
 import EditMeetupDetailsDialog from './components/Meetup/Edit/EditMeetupDetailsDialog'
+import EditMeetupDateDialog from './components/Meetup/Edit/EditMeetupDateDialog'
+import EditMeetupTimeDialog from './components/Meetup/Edit/EditMeetupTimeDialog'
 
 Vue.use(Vuetify)
 Vue.config.productionTip = false
@@ -14,6 +16,9 @@ Vue.config.productionTip = false
 Vue.filter('date', DateFilter)
 Vue.component('app-alert', AlertCmp)
 Vue.component('app-edit-meetup-details-dialog', EditMeetupDetailsDialog)
+Vue.component('app-edit-meetup-date-dialog', EditMeetupDateDialog)
+Vue.component('app-edit-meetup-time-dialog', EditMeetupTimeDialog)
+
 
 /* eslint-disable no-new */
 new Vue({
